4be3bb7cc40e0025a59fb30f091c2dd83ad8d70a
Victor Costa <vg47683@gmail.com>
2022-07-02 14:35:09 -0300

    Project created

---
+++ src/main/java/com/victorglcosta/coursemc/SpringBackendIonicApplication.java
@@ -0,0 +1,13 @@
+package com.victorglcosta.coursemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorglcosta/coursemc/SpringBackendIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorglcosta.coursemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
