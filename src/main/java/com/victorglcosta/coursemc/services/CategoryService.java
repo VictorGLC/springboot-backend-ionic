344d5c5546ffeeeef497d79d128a24758bc51b34
Victor Costa <vg47683@gmail.com>
2022-08-05 14:53:22 -0300

    Endpoint adjustments /categories{id}

--- src/main/java/com/victorglcosta/coursemc/services/CategoryService.java
+++ src/main/java/com/victorglcosta/coursemc/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.victorglcosta.coursemc.domain.Category;
 import com.victorglcosta.coursemc.repositories.CategoryRepository;
+import com.victorglcosta.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,6 +17,7 @@ public class CategoryService {
 	
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
 }
